ea7b2e113387d71251d2b458f044f98a7b9f21fd
DESKTOP-E6NHFJG\Binh <binhthe@gmail.com>
2025-07-08 17:01:59 +0700

    V1.0.25.07.08
    -Update chapter 5

---
+++ Demo_chapter5/BlazingTrails/BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+
+namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class TrailDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int TimeInMinutes { get; set; }
+    public int Length { get; set; }
+    public List<RouteInstruction> Route { get; set; } = new List<RouteInstruction>();
+
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
+
+public class TrailValidator : AbstractValidator<TrailDto>
+{
+    public TrailValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
+        RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location");
+        RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length");
+        RuleForEach(x => x.Route).SetValidator(new RouteInstructionValidator());
+    }
+}
+
+public class RouteInstructionValidator : AbstractValidator<TrailDto.RouteInstruction>
+{
+    public RouteInstructionValidator()
+    {
+        RuleFor(x => x.Stage).NotEmpty().WithMessage("Please enter a stage");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
+    }
+}
