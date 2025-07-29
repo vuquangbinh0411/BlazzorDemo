adf714ed5979cc5ae83053f07757548f0513c138
DESKTOP-E6NHFJG\Binh <binhthe@gmail.com>
2025-07-29 09:38:48 +0700

    V1.01.25.07.25

---
+++ Demo_chapter8/BlazingTrails/BlazingTrails.Api/Program.cs
@@ -0,0 +1,36 @@
+using BlazingTrails.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<BlazingTrailsContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("BlazingTrailsContext")));
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.Load("BlazingTrails.Shared")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Images")),
+    RequestPath = new Microsoft.AspNetCore.Http.PathString("/Images")
+});
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
---
+++ Demo_chapter8/BlazingTrails/BlazingTrails.Client/Features/ManageTrails/EditTrail/GetTrailHandler.cs
@@ -0,0 +1,27 @@
+﻿using BlazingTrails.Shared.Features.ManageTrails.EditTrail;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace BlazingTrails.Client.Features.ManageTrails.EditTrail;
+
+public class GetTrailHandler : IRequestHandler<GetTrailRequest, GetTrailRequest.Response?>
+{
+    private readonly HttpClient _httpClient;
+
+    public GetTrailHandler(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<GetTrailRequest.Response?> Handle(GetTrailRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await _httpClient.GetFromJsonAsync<GetTrailRequest.Response>(GetTrailRequest.RouteTemplate.Replace("{trailId}", request.TrailId.ToString()));
+        }
+        catch (HttpRequestException)
+        {
+            return default!;
+        }
+    }
+}
